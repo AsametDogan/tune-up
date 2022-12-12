68a8f0d958b7f46e750a340cc9060a0e5e7e302b
Ahmetalper1 <fake_polt@hotmail.com>
2022-12-12 22:45:29 +0300

    explore

--- src/components/MusicPost.js
+++ src/components/MusicPost.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function MusicPost({ user }) {
+function MusicPost({ music }) {
 
     //id: ownerId:  name: description: data: createdAt: color: liker: unliker: isPublic:
 
@@ -9,7 +9,7 @@ function MusicPost({ user }) {
     return (
         <div className='w-full flex flex-col rounded-md px-3 border py-4 shadow-sm bg-white hover:shadow transition-all duration-300'>
             <div className=''>
-
+        {music.description}
             </div>
         </div>
     )
